ccd5a97729fffdfbc7b7bac937a9887e81c060f3
NoahH <NoahH@DESKTOP-JAEBKSV>
2024-09-18 09:36:54 +0200

    Add project files.

---
+++ ChessBoard/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ChessBoard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hejsan, dags att välja hur lång sidan på din schackbräda skall vara: ");
+            int boardLength = Convert.ToInt32(Console.ReadLine());
+            string evenNumber = "";
+            string oddNumber = "";
+            int[,] board = new int[boardLength, boardLength];
+        }
+    }
+}
